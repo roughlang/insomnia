45c3f14fa251f4541dafa86d9f0d1a4b0f52b57f
山上 修 <roughlangx@gmail.com>
2022-04-26 14:19:27 +0900

    wp update

--- app/resources/views/include/footer.blade.php
+++ app/resources/views/include/footer.blade.php
@@ -1,3 +1,31 @@
+
+
+<script>
+$(function(){
+  var pagetop = $('#page_top');
+  // ボタン非表示
+  pagetop.hide();
+  // 100px スクロールしたらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
+</script>
+<div class="topback_button" id="page_top">
+  <div class="topback_img">
+    <svg xmlns="/assets/img/item/arrow-up-square-fill.svg" width="32" height="32" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+      <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+    </svg>
+  </div>
+</div>
 <footer class="footer hr">
 	<div class="copy-right text-align-center mt20 mb20">
 		<p class="text-center">© 2021 Roughlang, INC. All Rights Reserved</p>
